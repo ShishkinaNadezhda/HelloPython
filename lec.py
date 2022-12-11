28bdbb41d7dffb51cf0d106928af23e07c27c9a1
ShishkinaNadezhda <79129261498@yandex.ru>
2022-12-11 21:35:21 +0300

    lec.py

--- lec.py
+++ lec.py
@@ -3,15 +3,36 @@ print('hello world')
 # типы данных и переменные:
 # int, str, boolean, float, list, none
 
-value =None
-print(type(value))
+value = None
+print(value)
 a = 123
 b = 1.23
-print(type(a))
-print(type(b))
+print(a)
+print(b)
+#print(type(b))
 
 value = 12334
-print(type(value))
+print(value)
 
-s = ('hello world')
-print(s) # вывод строки
+s = 'hello world'
+# print(s) # вывод строки
+
+print(a, '-', b, '-',  s)
+print('{1} - {2} - {0}'.format(a, b, s))
+print(f'{a} - {b} - {s}')
+
+F = True
+print(F)
+
+list = ['1', '2', '3', 'hello']
+print(list)
+
+# ввод и вывод данных
+
+print('Введите a');
+a = int(input())
+print('Введите b');
+b = int(input())
+print(a, '+', b, '=', a+b)
+# print('{} {}'.format(a, b))
+# print(f'{a} {b}')
