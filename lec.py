32545ca365d2ea5228f1c357e6acf70e9153cedd
ShishkinaNadezhda <79129261498@yandex.ru>
2022-12-11 21:02:32 +0300

    lec.ru

---
+++ lec.py
@@ -0,0 +1,17 @@
+print('hello world')
+
+# типы данных и переменные:
+# int, str, boolean, float, list, none
+
+value =None
+print(type(value))
+a = 123
+b = 1.23
+print(type(a))
+print(type(b))
+
+value = 12334
+print(type(value))
+
+s = ('hello world')
+print(s) # вывод строки
