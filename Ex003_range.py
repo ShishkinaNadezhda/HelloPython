e74b0a797bc8d0083d0814934d2a1629b218613d
ShishkinaNadezhda <79129261498@yandex.ru>
2022-12-17 19:30:26 +0300

    Ex003_range

---
+++ Ex003_range.py
@@ -0,0 +1,6 @@
+# массив в пайтоне
+
+# range(5) -> range(start=0, stop=5, step=1)
+# range(1,5) -> range(start=1, stop=5, step=1)
+# range(1,9,2) range(start=1, stop=9, step=2)
+# range(9,1,-1) range(start=9, stop=1, step=-1)
