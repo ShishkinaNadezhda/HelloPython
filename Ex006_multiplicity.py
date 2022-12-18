fa206bda4c497dadbae463d197dc52b2b8ef89ae
ShishkinaNadezhda <79129261498@yandex.ru>
2022-12-18 14:00:11 +0300

    Ex006_multiplicity

---
+++ Ex006_multiplicity.py
@@ -0,0 +1,18 @@
+# Напишите программу, которая принимает на вход число и проверяет, 
+# кратно ли оно 5 и 10 или 15, но не 30.
+
+# первый вариант:
+
+# num = int(input('Enter number: '))
+# if num % 5 == 0 and num % 10 == 0 or num % 15 == 0 or num != 30:
+#    print('yes')
+#else:
+#    print('no')
+
+
+
+user_number = int(input('Enter your number: '))
+if (user_number % 5 == 0 and user_number % 10 == 0 or user_number % 15 == 0) and user_number % 30 != 0:
+    print('Your number is nice! :)')
+else:
+    print('Try again.')
